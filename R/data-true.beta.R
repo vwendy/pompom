8d78bc9147346f22c350da007b3140adec50dbe7
vwendy <vwendy@gmail.com>
2017-12-20 15:18:56 -0500

    Included true beta matrix and simulated time series into the Rdata and made data R files.

---
+++ R/data-true.beta.R
@@ -0,0 +1,27 @@
+
+#' The true beta matrix (6 by 6) used in simulation
+#'true.beta <-  matrix(c(0,0,0,0,0,0,
+#'                       0,0,0,0,0,0,
+#'                       0,0,0,0,0,0,
+#'                       0.2,0,0.25,0,0,0.6,
+#'                       0,0.3,0,-0.2,0,-0.6,
+#'                       0,-0.2,0.3,0,0,0),
+#'                      nrow = 6,
+#'                      ncol = 6,
+#'                      byrow = TRUE)
+#'
+#'
+#'
+#' @docType data
+#'
+#' @usage true.beta
+#'
+#'
+#' @keywords true.beta
+#'
+#' @examples
+#' data(true.beta)
+#' \donttest{
+#'true.beta
+#'  }
+"true.beta"
