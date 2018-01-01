dcbaeb3c4dea27a799c2a2424e8d0652bcbbd5ac
vwendy <vwendy@gmail.com>
2018-01-01 16:24:42 -0500

    Adding data files to assist examples (reducing running time)

---
+++ R/data-model.fit.R
@@ -0,0 +1,20 @@
+
+#' Model fitbased on similated time-series by uSEM.
+#'
+#'
+#'
+#' @docType data
+#'
+#' @usage model.fit
+#'
+#' @examples
+#' data(model.fit)
+#' \dontshow{
+#'model.fit
+#'  }
+#' \donttest{
+#'model.fit
+#'  }
+#'
+
+"model.fit"
