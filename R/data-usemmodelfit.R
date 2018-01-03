65327bd6e08a4ff841bf643117fea1e8643c89c3
vwendy <vwendy@gmail.com>
2018-01-02 21:08:04 -0500

    Save model.fit object to be included in the package.

---
+++ R/data-usemmodelfit.R
@@ -0,0 +1,20 @@
+
+#' Model fitbased on similated time-series by uSEM.
+#'
+#'
+#'
+#' @docType data
+#'
+#' @usage usemmodelfit
+#'
+#' @examples
+#'
+#' \dontshow{
+#'data(usemmodelfit)
+#'  }
+#' \donttest{
+#'data(usemmodelfit)
+#'  }
+#'
+
+"usemmodelfit"
