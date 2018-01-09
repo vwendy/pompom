07655e48310781778fdcb228e24b6cb78f757c79
vwendy <vwendy@gmail.com>
2018-01-09 14:15:57 -0500

    Added several datasets of a 2-node network to shorten the running time of uSEM and plot_time_profile

---
+++ R/data-simts2node.R
@@ -0,0 +1,20 @@
+#' Simulated time-series data
+#'
+#'@details Data simulated from a given three-node network structure with 200 measurements.
+#'         Network structure is shown in the dataset true.beta.
+#'         Process noise has mean of 0 and SD .1.
+#'
+#' @docType data
+#'
+#' @usage simts2node
+#'
+#'
+#' @examples
+#' \dontshow{
+#' data(simts2node)
+#' \donttest{
+#' data(simts2node)
+#' }
+#'
+
+"simts2node"
---
+++ R/data-true.beta.2node.R
@@ -0,0 +1,28 @@
+
+#' The true beta matrix (4 by 4) used in simulation.
+#'
+#' @details true.beta.2node <-  matrix(c(0,0,0,0,
+#'                                 0,0,0,0,
+#'                                0.2,-.4,0,-0.25,
+#'                                0,0.3,-0.2,0),
+#'                      nrow = 4,
+#'                      ncol = 4,
+#'                      byrow = TRUE)
+#'
+#'
+#'
+#' @docType data
+#'
+#' @usage true.beta.2node
+#'
+#' @examples
+#' data(true.beta.2node)
+#' \dontshow{
+#'true.beta.2node
+#'  }
+#' \donttest{
+#'true.beta.2node
+#'  }
+#'
+
+"true.beta.2node"
