1c0c351f1f3df374ae9a33b2fc88e755c154125b
vwendy <vwendy@gmail.com>
2018-01-10 14:01:52 -0500

    change dot to underscore in function and dataset names

---
+++ R/data-simts_2node.R
@@ -0,0 +1,21 @@
+#' Simulated bivariate time-series data
+#'
+#'@details Data simulated from a given three-node network structure with 200 measurements.
+#'         Network structure is shown in the dataset true.beta.
+#'         Process noise has mean of 0 and SD .1.
+#'
+#' @docType data
+#'
+#' @usage simts_2node
+#'
+#'
+#' @examples
+#' \dontshow{
+#' data(simts_2node)
+#' }
+#' \donttest{
+#' data(simts_2node)
+#' }
+#'
+
+"simts_2node"
---
+++ R/data-simts_3node.R
@@ -0,0 +1,22 @@
+#' Simulated 3-variate time-series data
+#'
+#'@details Data simulated from a given three-node network structure with 200 measurements.
+#'         Network structure is shown in the dataset true.beta.
+#'         Process noise has mean of 0 and SD .1.
+#'
+#' @docType data
+#'
+#' @usage simts_3node
+#'
+#'
+#' @examples
+#' \dontshow{
+#' data(simts_3node)
+#'  }
+#' \donttest{
+#' data(simts_3node)
+#'  }
+#'
+
+
+"simts_3node"
